fb2c8f9998fbe307924c7d1fba767cc0a407e935
Yannick <17yannick17@gmail.com>
2026-02-18 18:06:38 -0500

    fix: Confirmation de suppression depuis la page d'accueil

--- app/src/main/java/com/example/studywisely/presentation/list/RoutinesListScreen.kt
+++ app/src/main/java/com/example/studywisely/presentation/list/RoutinesListScreen.kt
@@ -6,10 +6,19 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.studywisely.data.model.RoutineVM
 import com.example.studywisely.navigation.Screen
 import com.example.studywisely.presentation.components.RoutineCard
 import com.example.studywisely.ui.theme.*
@@ -26,6 +35,9 @@ fun RoutinesListScreen(
     LaunchedEffect(Unit) {
         viewModel.refresh()
     }
+
+    var showDeleteDialog by remember { mutableStateOf(false) }
+    var routineToDelete by remember { mutableStateOf<RoutineVM?>(null) }
 
     Scaffold(
         containerColor = BackgroundMain,
@@ -59,7 +71,6 @@ fun RoutinesListScreen(
                             }
                         }
 
-
                         Spacer(modifier = Modifier.height(12.dp))
                     }
                 }
@@ -79,11 +90,53 @@ fun RoutinesListScreen(
             items(routines, key = { it.id }) { routine ->
                 RoutineCard(
                     routine = routine,
-                    onDelete = { id ->
-                        viewModel.deleteRoutine(id)
+                    onDelete = {
+                        routineToDelete = routine
+                        showDeleteDialog = true
                     }
                 )
             }
+        }
+
+        if (showDeleteDialog && routineToDelete != null) {
+            AlertDialog(
+                onDismissRequest = {
+                    showDeleteDialog = false
+                    routineToDelete = null
+                },
+                text = {
+                    Text("Voulez-vous vraiment supprimer la routine \"${routineToDelete!!.title}\" ?",
+                        style= TextStyle(textAlign = TextAlign.Center),
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold)
+                },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            viewModel.deleteRoutine(routineToDelete!!.id)
+                            showDeleteDialog = false
+                            routineToDelete = null
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = PurpleMain)
+                    ) {
+                        Text("Oui",
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.Bold)
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            showDeleteDialog = false
+                            routineToDelete = null
+                        }
+                    ) {
+                        Text("Non",
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.Bold)
+                    }
+                }
+            )
         }
     }
 }
