9232a3d6cc6573202c765e66710f446e0b1f3fd9
Yannick <17yannick17@gmail.com>
2026-04-15 12:13:40 -0400

    Augmentation de la version de la base de données

--- app/src/main/java/com/example/studywisely/model/data/RoutinesDatabase.kt
+++ app/src/main/java/com/example/studywisely/model/data/RoutinesDatabase.kt
@@ -2,7 +2,7 @@ package com.example.studywisely.model.data
 
 import androidx.room.*
 
-@Database(entities = [RoutineEntity::class], version = 1)
+@Database(entities = [RoutineEntity::class], version = 2)
 @TypeConverters(Converters::class)
 abstract class RoutinesDatabase : RoomDatabase() {
     abstract val dao: RoutineDao
